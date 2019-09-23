20fbbce95cd0bec146eb275a6111c33dba02c854
wdkkou <koukouki7547@gmail.com>
2019-09-24 00:29:49 +0900

    現状バグあり

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,14 @@
-void io_hlt(void);
+extern void io_hlt(void);
+extern void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
-fin:
-    io_hlt();
-    goto fin;
+    for (int i = 0xa0000; i <= 0xaffff; i++)
+    {
+        write_mem8(i, 15);
+    }
+    for (;;)
+    {
+        io_hlt();
+    }
 }
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -3,7 +3,14 @@
 
 section .text
     GLOBAL  io_hlt
+    GLOBAL  write_mem8
 
 io_hlt:
     HLT
+    RET
+
+write_mem8:
+    MOV ECX, [ESP+4]
+    MOV AL, [ESP+8]
+    MOV [ECX], AL
     RET

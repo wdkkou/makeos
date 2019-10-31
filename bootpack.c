cf482c61bc6eb5512b622c6698da6043efa2f842
wdkkou <koukouki7547@gmail.com>
2019-10-31 14:33:55 +0900

    windowのタイトル部分を表示する関数の実装

--- bootpack.c
+++ bootpack.c
@@ -229,6 +229,21 @@ void HariMain(void)
 }
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
 {
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
+    boxfill8(buf, xsize, WHITE, 1, 1, xsize - 2, 1);
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
+    boxfill8(buf, xsize, WHITE, 1, 1, 1, ysize - 2);
+    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, WHITE, xsize - 1, 0, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
+    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, WHITE, 0, ysize - 1, xsize - 1, ysize - 1);
+    make_wtitle8(buf, xsize, title, act);
+
+    return;
+}
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
+{
     static char closebtn[14][16] = {
         "OOOOOOOOOOOOOOO@",
         "OQQQQQQQQQQQQQ$@",
@@ -256,18 +271,8 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
         tc = COL8_C6C6C6;
         tbc = COL8_848484;
     }
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, xsize - 1, 0);
-    boxfill8(buf, xsize, WHITE, 1, 1, xsize - 2, 1);
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0, 0, ysize - 1);
-    boxfill8(buf, xsize, WHITE, 1, 1, 1, ysize - 2);
-    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, WHITE, xsize - 1, 0, xsize - 1, ysize - 1);
-    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2, xsize - 3, ysize - 3);
     boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
-    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, WHITE, 0, ysize - 1, xsize - 1, ysize - 1);
     putfont8_asc(buf, xsize, 24, 4, tc, title);
-
     for (int y = 0; y < 14; y++)
     {
         for (int x = 0; x < 16; x++)
@@ -292,6 +297,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
             buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
         }
     }
+
     return;
 }
 

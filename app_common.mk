2a53e750155d48ad9fa94d837c252fd0c8c1ffc7
wdkkou <koukouki7547@gmail.com>
2020-05-12 16:50:32 +0900

    アプリのmakefileでのバグを修正

--- app_common.mk
+++ app_common.mk
@@ -5,13 +5,17 @@ CFLAGS := -c -m32 -march=i486
 default : $(APP).bin
 
 $(APP).bin : $(APP).o ./../apilib/apilib.lib $(OBJ_LIBRARIES) Makefile
-	gcc -m32 -march=i486 -nostdlib -fno-pic -T ../api.ls $(APP).o ../apilib/apilib.lib $(OBJ_LIBRARIES) -o $(APP).bin
+	# gcc -c -m32 -march=i486 -nostdlib -fno-pic -T ../api.ls $(APP).o ../apilib/apilib.lib $(OBJ_LIBRARIES) -o $(APP).bin
+	ld -m elf_i386 -e HariMain -n -T ../api.ls -static -o $@ $<  $(OBJ_LIBRARIES) ../apilib/apilib.lib
 
 %.o : %.asm Makefile
 	nasm -f elf $< -o $@
 
 %.o : %.c Makefile
-	$(CC) $(CFLAGS) $< -o $@
+	# $(CC) $(CFLAGS) $< -o $@
+	gcc -Wall -m32 -c -fno-pic -nostdlib -fno-builtin -I../ -I../haribote/ -o $@ $<
+	objdump -D $@ > $@.dmp
 
 clean :
-	rm *.bin *.o
+	rm *.o
+	rm *.bin
